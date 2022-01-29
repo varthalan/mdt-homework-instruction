fd75cc60910b5e3232e9058e77b7d20b6cf0eb50
Nagaraju <nvarthala@protonmail.com>
2022-01-29 22:05:51 +0800

    Invoking createAccount method sends request to register account with given username and password

--- DigibankLight/DigibankLightTests/Account/Registration/RegistrationServiceTests.swift
+++ DigibankLight/DigibankLightTests/Account/Registration/RegistrationServiceTests.swift
@@ -6,23 +6,52 @@
 import XCTest
 @testable import DigibankLight
 
+struct RegistrationResponse {
+    
+}
+
+final class RegistrationService {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func createAccount(for username: String, password: String, completion: @escaping (Swift.Result<RegistrationResponse, Error>) -> Void) {
+        client.load(request: request()) { result in
+            
+        }
+    }
+    
+    private func request() -> URLRequest {
+        URLRequest(url: url)
+    }
+}
+
 class RegistrationServiceTests: XCTestCase {
 
     func test_init_doesNotSendRegistrationRequest() {
-        let client = HTTPClientSpy()
+        let (_, client) = makeSUT()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
     }
     
+    func test_createAccount_sendsRequestToRegisterAccount() {
+        let url = URL(string: "https://any-url.com/register")!
+        let (sut, client) = makeSUT(url)
+        
+        sut.createAccount(for: "an username", password: "a password") { _ in }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
+    }
+    
     //MARK: - Helpers
     
-    final class HTTPClientSpy: HTTPClient {
-        
-        var requestedURLs = [URL]()
-        
-        func load(request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) {
-            
-        }
-        
+    private func makeSUT(_ url: URL = URL(string: "https://any-url.com")!) -> (sut: RegistrationService, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RegistrationService(url: url, client: client)
+        return (sut, client)
     }
 }
