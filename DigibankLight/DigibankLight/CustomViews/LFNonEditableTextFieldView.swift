3e59a415fb5a16710ca4aefa0501323871f8f1bd
Nagaraju <nvarthala@protonmail.com>
2022-01-29 14:26:09 +0800

    Add an arrow image at the end of Payee field

--- DigibankLight/DigibankLight/CustomViews/LFNonEditableTextFieldView.swift
+++ DigibankLight/DigibankLight/CustomViews/LFNonEditableTextFieldView.swift
@@ -7,16 +7,34 @@ import UIKit
 
 final class LFNonEditableTextFieldView: LFTextFieldView {
     
+    private let arrowImageView: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "arrow"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override func setupUI() {
         super.setupUI()
         textField.delegate = self
-        feedbackLabel.isHidden = true
+
+        setupArrowImage()
     }
     
     func setText(_ text: String) {
         textField.text = text
     }
     
+    
+    private func setupArrowImage() {
+        textField.addSubview(arrowImageView)
+        
+        NSLayoutConstraint.activate([
+            arrowImageView.trailingAnchor.constraint(equalTo: textField.trailingAnchor),
+            arrowImageView.widthAnchor.constraint(equalToConstant: 15.0),
+            arrowImageView.heightAnchor.constraint(equalToConstant: 15.0),
+            arrowImageView.centerYAnchor.constraint(equalTo: textField.centerYAnchor)
+        ])
+    }
 }
 
 extension LFNonEditableTextFieldView {
