6d92c76b7ca7c1b8a7f0257205db51999f32a8b3
Nagaraju <nvarthala@protonmail.com>
2022-01-29 16:31:14 +0800

    Move strings to Localizable.strings file

---
+++ DigibankLight/DigibankLight/Modules/Dashboard/ViewModel/DashboardViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardViewModel.swift
+//  DigibankLight
+//
+
+import Foundation
+
+final class DashboardViewModel {
+    init() {}
+}
+
+//MARK: - Strings
+extension DashboardViewModel {
+    
+    static var logoutButtonTitle: String {
+        localize("logout_button_title")
+    }
+
+    static var makeTransferButtonTitle: String {
+        localize("make_transfer_button_title")
+    }
+
+    
+}
