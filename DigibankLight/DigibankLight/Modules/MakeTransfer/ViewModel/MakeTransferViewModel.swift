970f2af310bdff4c4d10445dda0abf36eff015a1
Nagaraju <nvarthala@protonmail.com>
2022-01-29 16:36:51 +0800

    Create view model and move strings to view model

---
+++ DigibankLight/DigibankLight/Modules/MakeTransfer/ViewModel/MakeTransferViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  MakeTransferViewModel.swift
+//  DigibankLight
+//
+
+import Foundation
+
+final class MakeTransferViewModel {
+    init() {}
+}
+
+//MARK: - Strings
+extension MakeTransferViewModel {
+    
+    static var transferTitle: String {
+        "Transfer"
+    }
+
+    static var payeeFieldTitle: String {
+        "Payee"
+    }
+
+    static var amountFieldTitle: String {
+        "Amount"
+    }
+
+    static var descriptionFieldTitle: String {
+        "Description"
+    }
+    
+    static var transferNowButtonTitle: String {
+        "Transfer Now"
+    }
+    
+}
