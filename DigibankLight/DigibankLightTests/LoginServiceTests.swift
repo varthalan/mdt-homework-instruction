78fa41caeb383f6a994a64ea19965de2ce75189a
Nagaraju <nvarthala@protonmail.com>
2022-01-29 19:33:03 +0800

    Add an empty unit test case

---
+++ DigibankLight/DigibankLightTests/LoginServiceTests.swift
@@ -0,0 +1,15 @@
+//
+//  LoginServiceTests.swift
+//  DigibankLightTests
+//
+//  Created by Nagaraju on 29/1/22.
+//
+
+import XCTest
+
+class LoginServiceTests: XCTestCase {
+    
+    func emptyTest() {
+        
+    }
+}
