a0ed9ac99c29adda55be78745a2f276d243abff1
Nagaraju <nvarthala@protonmail.com>
2022-01-29 19:36:58 +0800

    Initializing HTTPClient does not send login request

--- DigibankLight/DigibankLightTests/LoginServiceTests.swift
+++ DigibankLight/DigibankLightTests/LoginServiceTests.swift
@@ -6,10 +6,24 @@
 //
 
 import XCTest
+@testable import DigibankLight
 
 class LoginServiceTests: XCTestCase {
     
-    func emptyTest() {
+    func test_init_doesNotSendLoginRequest() {
+        let client = HTTPClientSpy()
         
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+
+    //MARK: - Helper
+    
+    private final class HTTPClientSpy: HTTPClient {
+        var requestedURLs = [URL]()
+        
+        func load(request: URLRequest, completion: @escaping (HTTPClient.Result) -> Void) {
+        }
+
     }
 }
