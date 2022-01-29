40bde897f21814e1edb517d4e7d44ab4bc36b7f9
Nagaraju <nvarthala@protonmail.com>
2022-01-29 10:04:00 +0800

    Add Register and Login buttons

--- DigibankLight/DigibankLight/Modules/Account/Login/LoginViewController.swift
+++ DigibankLight/DigibankLight/Modules/Account/Login/LoginViewController.swift
@@ -18,7 +18,19 @@ class LoginViewController: UIViewController {
         field.translatesAutoresizingMaskIntoConstraints = false
         return field
     }()
-    
+
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    private let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -51,8 +63,43 @@ extension LoginViewController {
         passwordField.setFieldType(.secured)
     }
     
+    private func setupRegisterButton() {
+        view.addSubview(registerButton)
+        NSLayoutConstraint.activate([
+            registerButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32.0),
+            registerButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -32.0),
+            registerButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40.0),
+            registerButton.heightAnchor.constraint(equalToConstant: 70.0)
+        ])
+        registerButton.setTitle("REGISTER", for: .normal)
+        registerButton.setTitleColor(.black, for: .normal)
+        registerButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .black)
+        registerButton.layer.cornerRadius = 35.0
+        registerButton.layer.borderWidth = 2.0
+        registerButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    private func setupLoginButtton() {
+        view.addSubview(loginButton)
+        NSLayoutConstraint.activate([
+            loginButton.leadingAnchor.constraint(equalTo: registerButton.leadingAnchor),
+            loginButton.widthAnchor.constraint(equalTo: registerButton.widthAnchor),
+            loginButton.bottomAnchor.constraint(equalTo: registerButton.topAnchor, constant: -20.0),
+            loginButton.heightAnchor.constraint(equalTo: registerButton.heightAnchor)
+        ])
+        loginButton.setTitle("LOGIN", for: .normal)
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.backgroundColor = .black
+        loginButton.titleLabel?.font = .systemFont(ofSize: 20, weight: .black)
+        loginButton.layer.cornerRadius = 35.0
+        loginButton.layer.borderWidth = 2.0
+        loginButton.layer.borderColor = UIColor.black.cgColor
+    }
+    
     private func setupUI() {
         setupUsernameField()
         setupPasswordField()
+        setupRegisterButton()
+        setupLoginButtton()
     }
 }
