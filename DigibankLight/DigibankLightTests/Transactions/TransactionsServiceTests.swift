6d4d9e32f156b25066a40cf47798fd9f5d6d2059
Nagaraju <nvarthala@protonmail.com>
2022-01-30 16:31:47 +0800

    TransactionsService.load loads transactions successfully with valid token

--- DigibankLight/DigibankLightTests/Transactions/TransactionsServiceTests.swift
+++ DigibankLight/DigibankLightTests/Transactions/TransactionsServiceTests.swift
@@ -8,37 +8,99 @@ import XCTest
 
 struct TransactionsResponse: Equatable {
     
+    struct Transaction: Equatable {
+        let transactionId: String?
+        let amount: Int?
+        let transactionDate: Date?
+        let description: String?
+        let transactionType: String?
+        let accountNumber: String?
+        let accountName: String?
+    }
+    
     struct Error: Equatable {
-        let name: String
-        let message: String
-        let tokenExpiredDate: String
+        let name: String?
+        let message: String?
+        let tokenExpiredDate: String?
     }
     
     let status: String
-    let error: Error
+    let transactions: [Transaction]?
+    let error: Error?
 }
+
+extension String {
+    func toDate() -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        formatter.locale = Locale(identifier: "en_GB")
+        return formatter.date(from: self)
+    }
+}
+
 
 final class TransactionsServiceMapper {
     
     private struct Result: Decodable {
         
+        private struct Transaction: Decodable {
+            
+            struct Receipient: Decodable {
+                let accountNo: String?
+                let accountHolder: String?
+            }
+            
+            struct Sender: Decodable {
+                let accountNo: String?
+                let accountHolder: String?
+            }
+            
+            let transactionId: String?
+            let amount: Int?
+            let transactionDate: String?
+            let description: String?
+            let transactionType: String?
+            let receipient: Receipient?
+            let sender: Sender?
+        }
+        
         private struct Error: Decodable {
-            let name: String
-            let message: String
-            let expiredAt: String
+            let name: String?
+            let message: String?
+            let expiredAt: String?
         }
         
         private let status: String
-        private let error: Error
+        private let data: [Transaction]?
+        private let error: Error?
         
         var transactionsResponse: TransactionsResponse {
-            TransactionsResponse(
+            let transactions = data?.map({ transaction -> TransactionsResponse.Transaction in
+                let transactionType = transaction.transactionType
+                var accountNumber: String?
+                var accountName: String?
+                if transactionType != nil && transactionType! == "transfer" {
+                    accountNumber = transaction.receipient?.accountNo
+                    accountName = transaction.receipient?.accountHolder
+                } else {
+                    accountNumber = transaction.sender?.accountNo
+                    accountName = transaction.sender?.accountHolder
+                }
+
+                return TransactionsResponse.Transaction(
+                    transactionId: transaction.transactionId,
+                    amount: transaction.amount,
+                    transactionDate: transaction.transactionDate?.toDate(),
+                    description: transaction.description,
+                    transactionType: transaction.transactionType,
+                    accountNumber: accountNumber,
+                    accountName: accountName
+                )
+            })
+            return TransactionsResponse(
                 status: status,
-                error: .init(
-                    name: error.name,
-                    message: error.message,
-                    tokenExpiredDate: error.expiredAt)
-            )
+                transactions: transactions,
+                error: .init(name: error?.name, message: error?.message, tokenExpiredDate: error?.expiredAt))
         }
     }
     
@@ -114,7 +176,7 @@ class TransactionsServiceTests: XCTestCase {
         let expectedResult = TransactionsService.Result.success(failedResponse)
         
         let exp = expectation(description: "Wait for loading transactions")
-        sut.load(token: "any token") { receivedResult in
+        sut.load(token: "any expired token") { receivedResult in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedResponse), .success(expectedResponse)):
                 XCTAssertEqual(receivedResponse, expectedResponse)
@@ -129,6 +191,49 @@ class TransactionsServiceTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_successfullyLoadsTransactionsWithValidToken() {
+        let url = URL(string: "https://any-url.com/transactions")!
+        let (sut, client) = makeSUT(url)
+        let transaction1 = makeTransaction(
+            transactionId: "a transaction id",
+            amount: 100,
+            transactionDate: "a transaction date",
+            description: "a description",
+            transactionType: "received",
+            accountNumber: "an account number",
+            accountName: "an account name")
+        
+        let transaction2 = makeTransaction(
+            transactionId: "another transaction id",
+            amount: 10,
+            transactionDate: "another transaction date",
+            description: "another description",
+            transactionType: "transfer",
+            accountNumber: "another account number",
+            accountName: "another account name")
+
+        let (successResponse, json) = makeTransactionsResponse(
+            status: "success",
+            transactions: [transaction1, transaction2],
+            error: nil
+        )
+        let expectedResult = TransactionsService.Result.success(successResponse)
+        
+        let exp = expectation(description: "Wait for loading transactions")
+        sut.load(token: "any token") { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedResponse), .success(expectedResponse)):
+                XCTAssertEqual(receivedResponse, expectedResponse)
+                
+            default:
+                XCTFail("Expected \(expectedResult), got \(receivedResult)")
+            }
+            
+            exp.fulfill()
+        }
+        client.complete(with: makeJSON(with: json))
+        wait(for: [exp], timeout: 1.0)
+    }
     //MARK: - Helpers
     
     private func makeSUT(_ url: URL = URL(string: "https://any-url.com")!) -> (sut: TransactionsService, client: HTTPClientSpy) {
@@ -137,24 +242,62 @@ class TransactionsServiceTests: XCTestCase {
         return (sut, client)
     }
     
+    private func makeTransaction(
+        transactionId: String,
+        amount: Int,
+        transactionDate: String,
+        description: String,
+        transactionType: String,
+        accountNumber: String,
+        accountName: String
+    ) -> TransactionsResponse.Transaction {
+        TransactionsResponse.Transaction(
+            transactionId: transactionId,
+            amount: amount,
+            transactionDate: transactionDate.toDate(),
+            description: description,
+            transactionType: transactionType,
+            accountNumber: accountNumber,
+            accountName: accountName)
+    }
+
+    
     private func makeTransactionsResponse(
         status: String,
-        error: TransactionsResponse.Error
+    transactions: [TransactionsResponse.Transaction]? = nil,
+        error: TransactionsResponse.Error? = nil
     ) -> (response: TransactionsResponse, json: [String: Any]) {
-        let response = TransactionsResponse(
+        let transactionResponse = TransactionsResponse(
             status: status,
-            error: error
-        )
+            transactions: transactions,
+            error: .init(name: error?.name, message: error?.message, tokenExpiredDate: error?.tokenExpiredDate))
+        
+        let jsonTransactions: [[String: Any?]]? = transactions?.map({ transaction in
+            var jsonTransaction: [String: Any?] = [
+                "transactionId": transaction.transactionId,
+                "amount": transaction.amount,
+                "transactionDate": transaction.transactionDate,
+                "description": transaction.description,
+                "transactionType": transaction.transactionType
+            ]
+            let transactionType = transaction.transactionType == "received" ? "sender" : "receipient"
+            jsonTransaction[transactionType] = [
+                "accountNo": transaction.accountNumber,
+                "accountHolder": transaction.accountName
+            ]
+            return jsonTransaction
+        })
         
         let json: [String: Any] = [
             "status": status,
+            "data" : jsonTransactions,
             "error": [
-                "name": error.name,
-                "message": error.message,
-                "expiredAt": error.tokenExpiredDate
+                "name": error?.name,
+                "message": error?.message,
+                "expiredAt": error?.tokenExpiredDate
             ]
-        ]
+        ].compactMapValues { $0 }
         
-        return (response, json)
+        return (transactionResponse, json)
     }
 }
