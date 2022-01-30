1c6c9b9fe5eac1894e29d02d950831df77a6628b
Nagaraju <nvarthala@protonmail.com>
2022-01-30 15:59:05 +0800

    TransactionsService.load method sends request to load transactions

--- DigibankLight/DigibankLightTests/Transactions/TransactionsServiceTests.swift
+++ DigibankLight/DigibankLightTests/Transactions/TransactionsServiceTests.swift
@@ -4,6 +4,27 @@
 //
 
 import XCTest
+@testable import DigibankLight
+
+final class TransactionsService {
+    private let url: URL
+    private let client: HTTPClient
+    
+    init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load(token: String, completion: @escaping () -> Void) {
+        client.load(request: request()) { result in
+            
+        }
+    }
+    
+    private func request() -> URLRequest {
+        URLRequest(url: url)
+    }
+}
 
 class TransactionsServiceTests: XCTestCase {
 
@@ -11,5 +32,15 @@ class TransactionsServiceTests: XCTestCase {
         let client = HTTPClientSpy()
         
         XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    
+    func test_load_sendsRequestToLoadTransactions() {
+        let url = URL(string: "https://any-url.com/transactions")!
+        let client = HTTPClientSpy()
+        let sut = TransactionsService(url: url, client: client)
+        
+        sut.load(token: "any token") { }
+        
+        XCTAssertEqual(client.requestedURLs, [url])
     }
 }
