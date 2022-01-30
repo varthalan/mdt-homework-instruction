8328abf95ac77b4133d460fff27c11f26e0247fa
Nagaraju <nvarthala@protonmail.com>
2022-01-30 10:55:36 +0800

    Move RegistrationService and related classes to Production from Unit Test environment

--- DigibankLight/DigibankLightTests/Account/Registration/RegistrationServiceTests.swift
+++ DigibankLight/DigibankLightTests/Account/Registration/RegistrationServiceTests.swift
@@ -6,67 +6,6 @@
 import XCTest
 @testable import DigibankLight
 
-struct RegistrationResponse: Equatable {
-    let status: String
-    let jwtToken: String?
-    let error: String?
-}
-
-final class RegistrationServiceMapper {
-    
-    private struct Result: Decodable {
-        let status: String
-        let token: String?
-        let error: String?
-        
-        var response: RegistrationResponse {
-            RegistrationResponse(
-                status: status,
-                jwtToken: token,
-                error: error
-            )
-        }
-    }
-    
-    static func map(_ data: Data, from response: HTTPURLResponse) throws -> RegistrationResponse {
-        guard let registrationResponse = try? JSONDecoder().decode(Result.self, from: data) else {
-            throw NSError(domain: "Parsing error in Registration response", code: 0)
-        }
-        return registrationResponse.response
-    }
-}
-
-final class RegistrationService {
-    private let url: URL
-    private let client: HTTPClient
-    
-    typealias Result = Swift.Result<RegistrationResponse, Error>
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func createAccount(for username: String, password: String, completion: @escaping (Result) -> Void) {
-        client.load(request: request()) { result in
-            switch result {
-            case let  .success(value):
-                do {
-                    completion(.success(try  RegistrationServiceMapper.map(value.0, from: value.1)))
-                } catch {
-                    completion(.failure(error))
-                }
-            case let .failure(error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    private func request() -> URLRequest {
-        URLRequest(url: url)
-    }
-}
-
 class RegistrationServiceTests: XCTestCase {
 
     func test_init_doesNotSendRegistrationRequest() {
@@ -134,8 +73,6 @@ class RegistrationServiceTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
-
-    
     
     //MARK: - Helpers
     
