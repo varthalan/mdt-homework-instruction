b5f2a8519ecee8b8afe1b41f7772bebf418e5ba4
Nagaraju <nvarthala@protonmail.com>
2022-01-30 10:43:39 +0800

    createAccount delivers failure for existing username

--- DigibankLight/DigibankLightTests/Account/Registration/RegistrationServiceTests.swift
+++ DigibankLight/DigibankLightTests/Account/Registration/RegistrationServiceTests.swift
@@ -6,8 +6,30 @@
 import XCTest
 @testable import DigibankLight
 
-struct RegistrationResponse {
+struct RegistrationResponse: Equatable {
+    let status: String
+    let error: String
+}
+
+final class RegistrationServiceMapper {
     
+    private struct Result: Decodable {
+        let status: String
+        let error: String
+        
+        var response: RegistrationResponse {
+            RegistrationResponse(
+                status: status,
+                error: error)
+        }
+    }
+    
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> RegistrationResponse {
+        guard let registrationResponse = try? JSONDecoder().decode(Result.self, from: data) else {
+            throw NSError(domain: "Parsing error in Registration response", code: 0)
+        }
+        return registrationResponse.response
+    }
 }
 
 final class RegistrationService {
@@ -21,7 +43,16 @@ final class RegistrationService {
     
     func createAccount(for username: String, password: String, completion: @escaping (Swift.Result<RegistrationResponse, Error>) -> Void) {
         client.load(request: request()) { result in
-            
+            switch result {
+            case let  .success(value):
+                do {
+                    completion(.success(try  RegistrationServiceMapper.map(value.0, from: value.1)))
+                } catch {
+                    completion(.failure(error))
+                }
+            case let .failure(error):
+                completion(.failure(error))
+            }
         }
     }
     
@@ -47,6 +78,32 @@ class RegistrationServiceTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
+    func test_createAccount_deliversErrorForExistingUser() {
+        let url = URL(string: "https://any-url.com/register")!
+        let (sut, client) = makeSUT(url)
+        
+        let (failedResponse, json) = makeRegistrationResponse(
+            status: "failed",
+            error: "any error")
+        let expectedResponse = failedResponse
+        
+        let exp = expectation(description: "Wait for registration")
+        sut.createAccount(for: "an existing username", password: "a password") { actualResponse in
+            switch actualResponse {
+            case let .success(returnedResponse):
+                XCTAssertEqual(returnedResponse, expectedResponse)
+                
+            default:
+                XCTFail("Expected \(expectedResponse), got \(actualResponse)")
+            }
+            exp.fulfill()
+        }
+        client.complete(with: makeJSON(with: json))
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    
     //MARK: - Helpers
     
     private func makeSUT(_ url: URL = URL(string: "https://any-url.com")!) -> (sut: RegistrationService, client: HTTPClientSpy) {
@@ -54,4 +111,23 @@ class RegistrationServiceTests: XCTestCase {
         let sut = RegistrationService(url: url, client: client)
         return (sut, client)
     }
+    
+    private func makeRegistrationResponse(status: String, error: String) -> (response: RegistrationResponse, json: [String: String] ) {
+        let response = RegistrationResponse(
+            status: status,
+            error: error
+        )
+        
+        let json = [
+            "status": status,
+            "error": error
+        ]
+        
+        return (response, json)
+    }
+    
+    private func makeJSON(with json: [String: Any]) -> Data {
+        try! JSONSerialization.data(withJSONObject: json)
+    }
+
 }
