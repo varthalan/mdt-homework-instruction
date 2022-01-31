66efce940c49ef33264ad66937c6baa59e4c013c
Nagaraju <nvarthala@protonmail.com>
2022-01-31 13:46:05 +0800

    Implement PayeesViewModel to call API and fetch payees list

--- DigibankLight/DigibankLight/Modules/Payees/ViewModel/PayeesViewModel.swift
+++ DigibankLight/DigibankLight/Modules/Payees/ViewModel/PayeesViewModel.swift
@@ -6,7 +6,42 @@
 import Foundation
 
 final class PayeesViewModel {
-    init() {}
+    private let service: PayeesService
+    private let jwtToken: String
+    
+    typealias Observer<T> = (T) -> Void
+    
+    var onLoadingStateChange: Observer<Bool>?
+    var onPayees: Observer<[PayeesResponse.Payee]>?
+    var onError: Observer<String>?
+
+    
+    init(service: PayeesService, jwtToken: String) {
+        self.service = service
+        self.jwtToken = jwtToken
+    }
+
+    func loadPayees() {
+        onLoadingStateChange?(true)
+        service.load(jwtToken: jwtToken) { [weak self] result in
+            
+            guard let self = self else { return }
+            self.onLoadingStateChange?(false)
+            
+            switch result {
+            case let .success(response):
+                if let error = response.error,
+                   let message = error.message {
+                    self.onError?(message)
+                } else {
+                    self.onPayees?(response.payees ?? [])
+                }
+                    
+            case let .failure(error):
+                self.onError?(error.localizedDescription)
+            }
+        }
+    }
 }
 
 //MARK: - Strings
